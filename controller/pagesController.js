c4c983598225ec98c1fe30c2ca8266a4f2f2f39a
Abdullokh <abdullokh.karimov.2006@gmail.com>
2025-02-18 19:48:56 +0500

    git commit

--- controller/pagesController.js
+++ controller/pagesController.js
@@ -1,28 +1,32 @@
+let getAbout = (req, res) => {
+  res.render("about");
+  res.status(404);
+};
 
-exports.getAbout( (req, res) => {
-    res.render("about");
-    res.status(404);
-});
+let getCourses = (req, res) => {
+  res.render("courses");
+  res.status(404);
+};
 
-exports.getCourses( (req, res) => {
-    res.render("courses");
-    res.status(404);
+let getTeam = (req, res) => {
+  res.render("team");
+  res.status(404);
+};
 
-})
+let getTestimonial = (req, res) => {
+  res.render("testimonial");
+  res.status(404);
+};
 
-exports.getTeam( (req, res) => {
-    res.render("team");
-    res.status(404);
+let getContact = (req, res) => {
+  res.render("contact");
+  res.status(404);
+};
 
-})
-
-exports.getTestimonial( (req, res) => {
-    res.render("testimonial");
-    res.status(404);
-
-})
-
-exports.getContact( (req, res) => { 
-    res.render("contact");
-    res.status(404);
-})
+module.exports = {
+  getAbout,
+  getCourses,
+  getTeam,
+  getTestimonial,
+  getContact,
+};
