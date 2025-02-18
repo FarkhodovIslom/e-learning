406699e84c3ff26129a6f8dd4de693a580ee6872
Farkhodov Islom <111051883+FarkhodovIslom@users.noreply.github.com>
2025-02-18 14:45:19 +0500

    File directions changed

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const morgan = require("morgan");
 const ejs = require("ejs");
 const path = require("path");
+const env = require("dotenv").config();
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, views));
+app.set('views', path.join(__dirname, 'public'));
 
 app.get("/", (req, res) => {
-    res.render("")
+    res.render("index");
+})
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server started at ${port}`)
 })
